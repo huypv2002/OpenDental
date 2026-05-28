c19190284990355dcb8c1e40eb5405e547121ba1
mavanhuy30 <kh431248@gmail.com>
2026-05-28 10:51:38 +0700

    Restore restart autologon scheduler

--- sms-reminder-tool/scheduler-off.bat
+++ sms-reminder-tool/scheduler-off.bat
@@ -1,7 +1,9 @@
 @echo off
 setlocal
 
+rem Run this file as Administrator.
 set "TASK_PREFIX=LUK Dental SMS"
+set "AUTOLOGON_EXE=%~dp0Autologon64.exe"
 
 echo Removing LUK Dental SMS scheduled tasks...
 schtasks /Delete /TN "%TASK_PREFIX% - Enable Auto Login" /F >nul 2>nul
@@ -10,7 +12,7 @@ schtasks /Delete /TN "%TASK_PREFIX% - Start Monitoring" /F >nul 2>nul
 schtasks /Delete /TN "%TASK_PREFIX% - Lock Screen" /F >nul 2>nul
 schtasks /Delete /TN "%TASK_PREFIX% - Screen Off" /F >nul 2>nul
 
-echo Disabling Windows auto-login from older scheduler versions...
+echo Disabling Windows auto-login...
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 0 /f >nul 2>nul
 
 echo Done. The scheduler tasks are now off.
